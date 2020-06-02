65c84d773e542666808281dba7a3a69579ffd9dc
Rakesh Bhat <rakeshbhat9@gmail.com>
2020-06-02 12:34:40 +0100

    udpated graph

--- app.py
+++ app.py
@@ -26,26 +26,26 @@ st.markdown(f'''Data as of {str(d['Specimen date'].max()).split(" ")[0]}
 
 area_type = st.selectbox(
     'Select Area Type', d['Area type'].unique().tolist())
-
 d = d[d['Area type'] == area_type]
 
 select_area = st.selectbox(
     'Select your area', d['Area name'].unique().tolist())
+d = d[d['Area name'] == select_area]
 
 trigger = st.button('Get chart')
 
 
 if trigger:
-    st.subheader("Time series chart")
     fil_data = d[(d['Area type'] == area_type) &
-                 (d['Area name'] == select_area)]
+                (d['Area name'] == select_area)]
+    st.subheader(f'''Total cases as of {str(fil_data['Specimen date'].max()).split(" ")[0]} stands at {fil_data['Cumulative lab-confirmed cases'].max():,}''')
+
+    st.subheader("Time series chart")
     fig = plt.figure()
-    plt.plot(fil_data[['Specimen date', 'Daily lab-confirmed cases',
-                       'Cumulative lab-confirmed cases']].set_index('Specimen date'))
+    plt.plot(fil_data[['Specimen date', 'Daily lab-confirmed cases',]].set_index('Specimen date'))
     st.plotly_chart(fig)
 
-    st.subheader("Top 5 worse days for selected area")
-    
+    st.subheader("Peaks - Top 5 worse days for selected area")
     fig2 = plt.figure()
     fil_data.reset_index(inplace=True,drop=True)
     st.area_chart(fil_data.iloc[fil_data['Daily lab-confirmed cases'].nlargest(
